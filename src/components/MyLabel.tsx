cf19547a04318af40347636acc5e3815b8821dd6
EnzoAliatis <enzo.aliatis@gmail.com>
2023-01-21 14:23:02 -0500

    ready  to deploy

---
+++ src/components/MyLabel.tsx
@@ -0,0 +1,41 @@
+import './myLabel.css';
+
+export interface MyLabelProps {
+  /**
+   * This is the message to show
+   */
+  label: string;
+  /**
+   * This is the size of the description
+   */
+  size: 'normal' | 'h1' | 'h2' | 'h3';
+
+  /**
+   * Set true if you want the label all capitals
+   */
+  allCaps?: boolean;
+
+  /**
+   * Set type color
+   */
+  color?: 'primary' | 'secondary' | 'tertiary';
+  
+  /**
+   * Set custom color
+   */
+  customColor?: string;
+}
+
+export const MyLabel = ({
+  label = 'No Label',
+  size = 'normal',
+  allCaps = false,
+  color,
+  customColor,
+}: MyLabelProps) => {
+  return (
+    <span className={`label ${size}  text-${color}  ${allCaps && 'allCaps'}`}  style={{ color: customColor }}>
+      {label}
+    </span>
+  );
+};
